4cf2e39e18aa901a7ec518bcc628dbc4403045eb
omer-nevo <81370933+omer-nevo@users.noreply.github.com>
2021-06-12 11:28:53 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,9 @@
+import pandas
+
+
+class Data:
+
+    def __init__(self, path):
+        self.path = path
+        df = pandas.read_csv(path)  # import the path to df
+        self.data = df.to_dict(orient="list")  # converting the cvs to dictionary
