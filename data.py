6bd0f196b084eac9a6d0b57b5212b6206d903e2c
omern <omerneyvo@gmail.com>
2021-06-12 11:43:58 +0300

    s

--- data.py
+++ data.py
@@ -7,3 +7,4 @@ class Data:
         self.path = path
         df = pandas.read_csv(path)  # import the path to df
         self.data = df.to_dict(orient="list")  # converting the cvs to dictionary
+        print("good luck")
